3a596d4fea7a5f1ee227fba143ef07a8e7e27795
Om Pandey <mynameisompandey007@gmail.com>
2024-05-12 16:32:54 +0530

    somechanges

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect=async ()=>{
     try {
-      const connection=  await mongoose.connect(`mongodb+srv://Riddle:9118380538@cluster0.qr5vrpz.mongodb.net/samsungDB`)
+      const connection=  await mongoose.connect(`mongodb+srv://Riddle:<passowrdNahibataunga>@cluster0.qr5vrpz.mongodb.net/samsungDB`)
       console.log(`server has successfully connected to the database DB Host:${connection.connection.host}`)
     } catch (error) {
       console.log(`error in connecting to the database`)
