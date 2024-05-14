37baefd32a1e00fab4ae2ed4ab991530e43cea0f
Om Pandey <mynameisompandey007@gmail.com>
2024-05-14 21:34:05 +0530

    some changes

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-import connect from './db/index.js';
-import userModel from './models/user.model.js';
-import productModel from './models/product.model.js';
-import cors from 'cors'
-
-
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import connect from "./db/index.js";
+import userModel from "./models/user.model.js";
+import productModel from "./models/product.model.js";
+import cors from "cors";
 
 connect();
 
-const app =express()
-const PORT=3000
-const __dirname=path.resolve();
+const app = express();
+const PORT = 3000;
+const __dirname = path.resolve();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname,'public')))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/data", (req, res) => {
+  res.json({ msg: "This is CORS-enabled for a Single Route" });
+});
 
-app.get('/data',(req,res)=>{
-    res.json({"msg": 'This is CORS-enabled for a Single Route'})
-})
+app.post("/signup", async (req, res) => {
+  const { email, password, fullname, username } = req.body;
+  const user = await userModel.create({ email, password, fullname, username });
+  console.log(user);
+});
 
-
-app.listen(PORT,()=>{
-    console.log(`server is running on PORT: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on PORT: ${PORT}`);
+});
